d08875fbf45c5b6fcb8430248654ac2bd3fad05d
Timur Karimov <tkarimov@academic.rrc.ca>
2026-04-05 19:29:39 -0500

    preapration app and server for routes

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,20 @@
+import cors, { type CorsOptions } from "cors";
 import express, { type Express, type Request, type Response } from "express";
+import helmet from "helmet";
 
 const app: Express = express();
 
+const corsOptions: CorsOptions = {
+  origin: "http://localhost:3000",
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+};
+
+app.use(helmet());
+app.use(cors(corsOptions));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (_request: Request, response: Response) => {
   response.status(200).json({
@@ -10,4 +22,10 @@ app.get("/", (_request: Request, response: Response) => {
   });
 });
 
-export default app;
+app.use((_request: Request, response: Response) => {
+  response.status(404).json({
+    message: "Route not found",
+  });
+});
+
+export { app };
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import "dotenv/config";
-import app from "./app";
+import { app } from "./app";
 import { environmentConfiguration } from "./config/env";
 
 app.listen(environmentConfiguration.port, () => {
